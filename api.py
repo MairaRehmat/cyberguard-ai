bec0817e93ea03689cf929277b176e29feb494dd
Maira Rehmat <mairarehmat58@gmail.com>
2026-09-06 20:34:50 +0500

    Update api.py

--- api.py
+++ api.py
@@ -1,3 +1,13 @@
+import os
+import sys
+
+# Add src folder to Python path
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SRC_DIR = os.path.join(BASE_DIR, "src")
+
+if SRC_DIR not in sys.path:
+    sys.path.insert(0, SRC_DIR)
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from crew import check_message
